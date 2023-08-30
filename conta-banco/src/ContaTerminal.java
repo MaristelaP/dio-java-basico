f8fc8b57157817ed17a92ba2c5d356c5a0af39c9
MaristelaP <marispeg@gmail.com>
2023-08-30 14:24:21 -0300

    código alterado para formato usuário preenchendo os dados

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -4,28 +4,25 @@ import java.util.Locale;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
-        Integer numero = 1021;
-        String agencia = "067-8"; 
-        String nomeCliente = "Mario Andrade";
-        Double saldo = 237.48;   
-
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Por favor, digite o número da agência!");
-        numero = scanner.nextInt();
+        String agencia = scanner.next();
 
-        if(numero == 1021) {
-            System.out.println("Olá " + nomeCliente + 
+        System.out.println("Por favor, digite o número da conta :");
+        Integer numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o saldo : ");
+        Double saldo = scanner.nextDouble();       
+
+        
+        System.out.println("Olá " + nomeCliente + 
             ", obrigado por criar uma conta em nosso banco, sua agência é " +
-            agencia + " conta " + numero + " e seu saldo de R$ " + saldo + 
+            agencia + " conta " + numeroConta + " e seu saldo de R$ " + saldo + 
             " já está disponível para saque.");
-        } else {
-            System.out.println("Número incorreto, tente novamente!");
-        }
-
-        /*achei estranho pedir para o usuário digitar o número da agência
-        e a resposta do usário se 1021, mas não consegui fazer o if funcionar
-        com a resposta 067-8*/    
               
     }
 }
